065de679a0dc6c4ba615a9fc9033f03de85864f9
lhk <lhk@DESKTOP-GFDSDIH>
2020-01-13 16:51:04 +0900

    .

--- MongoREST/src/main/java/com/example/mongorest/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mongorest.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.mongorest.model.CarModel;
-
-public interface CarRepository extends MongoRepository<CarModel, String> { }
