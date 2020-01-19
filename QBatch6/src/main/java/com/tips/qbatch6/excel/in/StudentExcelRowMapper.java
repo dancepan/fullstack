7a153ce76e158b91ae2245eab2b29e064f5124e9
LHK <LHK@DESKTOP-EC5V0S5>
2020-01-20 08:22:27 +0900

    .

---
+++ QBatch6/src/main/java/com/tips/qbatch6/excel/in/StudentExcelRowMapper.java
@@ -0,0 +1,28 @@
+package com.tips.qbatch6.excel.in;
+
+import org.springframework.batch.item.excel.RowMapper;
+import org.springframework.batch.item.excel.support.rowset.RowSet;
+
+import com.tips.qbatch6.student.StudentDTO;
+
+/**
+ * This class demonstrates how we can implement a row mapper that maps
+ * a row found from an Excel document into a {@code StudentDTO} object. If
+ * the Excel document has no header, we have to use this method for transforming
+ * the input data into {@code StudentDTO} objects.
+ *
+ * @author Petri Kainulainen
+ */
+public class StudentExcelRowMapper implements RowMapper<StudentDTO> {
+
+    @Override
+    public StudentDTO mapRow(RowSet rowSet) throws Exception {
+        StudentDTO student = new StudentDTO();
+
+        student.setName(rowSet.getColumnValue(0));
+        student.setEmailAddress(rowSet.getColumnValue(1));
+        student.setPurchasedPackage(rowSet.getColumnValue(2));
+
+        return student;
+    }
+}
