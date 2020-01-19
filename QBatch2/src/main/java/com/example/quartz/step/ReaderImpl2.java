88703e3e0dd61220710f0ef208dd97347355bdbe
leedo <leedo@DESKTOP-1ES44L4>
2020-01-19 14:28:50 +0900

    .

--- QBatch2/src/main/java/com/example/quartz/step/ReaderImpl2.java
+++ QBatch2/src/main/java/com/example/quartz/step/ReaderImpl2.java
@@ -1,5 +1,7 @@
 package com.example.quartz.step;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemReader;
 
 import com.example.quartz.model.ReaderReturnDTO;
@@ -10,11 +12,22 @@ import java.util.List;
  
 public class ReaderImpl2 implements ItemReader<ReaderReturnDTO>
 {
+	private static final Logger log = LoggerFactory.getLogger(ReaderImpl2.class);
+	
+	ReaderReturnDTO readerReturnDTO = new ReaderReturnDTO();
+	
 	public ReaderImpl2() {};
 	
 	@Override
 	public ReaderReturnDTO read()
 	{
-		return null;
+		log.info("[ReaderImpl] read()");
+		
+		readerReturnDTO.setPrice("1");
+		readerReturnDTO.setShares("2");
+		readerReturnDTO.setStock("2");
+		readerReturnDTO.setTime("4");
+		
+		return readerReturnDTO;
 	}
 }
