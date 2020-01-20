9ea2824b6ca8bd3c09199a8d9061c267ab0a187d
lhk <lhk@DESKTOP-GFDSDIH>
2020-01-20 22:45:49 +0900

    .

---
+++ batch/src/main/java/com/tips/batch/QBatch2Application.java
@@ -0,0 +1,18 @@
+package com.tips.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Class Application.
+ * 
+ * @author ashraf
+ */
+@SpringBootApplication
+public class QBatch2Application
+{
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(QBatch2Application.class, args);
+    }
+}
